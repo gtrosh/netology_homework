76c16233cd7c8cc04f805116a90abb9f178a6002
Galina Troshkina <maystar2009@gmail.com>
2022-12-19 21:55:19 +0300

    Completed homework

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,17 +1,31 @@
+from csv import DictReader
+
+from django.conf import settings
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
 
 def index(request):
-    return redirect(reverse('bus_stations'))
+    return redirect(reverse("bus_stations"))
+
+
+file_path = settings.BUS_STATION_CSV
+with open(file_path, encoding="utf-8") as csvfile:
+    reader = DictReader(csvfile)
+    STATIONS = [
+        {"Name": row["Name"], "Street": row["Street"], "District": row["District"]}
+        for row in reader
+    ]
 
 
 def bus_stations(request):
-    # получите текущую страницу и передайте ее в контекст
-    # также передайте в контекст список станций на странице
-
+    page_num = request.GET.get("page", 1)
+    paginator = Paginator(STATIONS, 10)
+    page = paginator.get_page(page_num)
+    bus_stations = page.object_list
     context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
+        "bus_stations": bus_stations,
+        "page": page,
     }
-    return render(request, 'stations/index.html', context)
+    return render(request, "stations/index.html", context)
--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -1,30 +1,27 @@
 from django.shortcuts import render
 
 DATA = {
-    'omlet': {
-        'яйца, шт': 2,
-        'молоко, л': 0.1,
-        'соль, ч.л.': 0.5,
+    "omlet": {
+        "яйца, шт": 2,
+        "молоко, л": 0.1,
+        "соль, ч.л.": 0.5,
     },
-    'pasta': {
-        'макароны, г': 0.3,
-        'сыр, г': 0.05,
+    "pasta": {
+        "макароны, г": 0.3,
+        "сыр, г": 0.05,
     },
-    'buter': {
-        'хлеб, ломтик': 1,
-        'колбаса, ломтик': 1,
-        'сыр, ломтик': 1,
-        'помидор, ломтик': 1,
-    },
-    # можете добавить свои рецепты ;)
+    "buter": {
+        "хлеб, ломтик": 1,
+        "колбаса, ломтик": 1,
+        "сыр, ломтик": 1,
+        "помидор, ломтик": 1,
+    }
 }
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+
+def ingredients(request, dish: str):
+    servings = int(request.GET.get("servings", 1))
+    ingredients = DATA[dish]
+    recipe = {key: value * servings for key, value in ingredients.items()}
+    context = {"recipe": recipe}
+    return render(request, "calculator/index.html", context)
