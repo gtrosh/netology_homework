30022da3cc833eab7d8710618fc3f240619f4220
Galina Troshkina <maystar2009@gmail.com>
2023-01-08 17:02:32 +0300

    Completed homework

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,20 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+from django.urls import reverse
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(max_length=50)
+    image = models.URLField()
+    price = models.PositiveIntegerField()
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField(null=False, unique=True)
+
+    def get_absolute_url(self):
+        return reverse('phone', kwargs={'slug': self.slug})
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        return super().save(*args, **kwargs)
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 
+from .models import Phone
+
 
 def index(request):
     return redirect('catalog')
@@ -7,11 +9,21 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    sort = request.GET.get("sort", None)
+    phones = Phone.objects.all()
+    if sort == 'name':
+        phones = phones.order_by('name')
+    elif sort == 'min_price':
+        phones = phones.order_by('price')
+    elif sort == 'max_price':
+        phones = phones.order_by('-price')
+    context = {'phones': phones}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone = Phone.objects.get(slug=slug)
+    print(phone)
+    context = {'phone': phone}
     return render(request, template, context)
