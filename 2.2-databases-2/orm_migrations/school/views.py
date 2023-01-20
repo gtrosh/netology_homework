dabf64901bfb5c46d3b283a9cfe042f137ea906e
Galina Troshkina <maystar2009@gmail.com>
2023-01-20 13:48:58 +0300

    Deleted comments

--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -8,8 +8,4 @@ def students_list(request):
     template = 'school/students_list.html'
     students = Student.objects.all().prefetch_related('teachers')
     context = {'object_list': students}
-
-    # используйте этот параметр для упорядочивания результатов
-    # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
-
     return render(request, template, context)
